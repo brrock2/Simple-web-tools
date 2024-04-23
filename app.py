5e1f1bcb78717b4cd51db727081a8c3a3ea18bfe
brrock <123623302+brrock@users.noreply.github.com>
2024-04-23 07:19:00 +0100

    starter

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Test")
+    return render_template("index.html", title="Home |Simple web Tools")
